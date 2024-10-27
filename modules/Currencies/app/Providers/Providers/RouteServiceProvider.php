dab3375bfa34c7f50cb8d82f599e5ff96f9610ec
warlyn <warlyn200@gmail.com>
2024-10-27 11:58:49 -0400

    samples modules

---
+++ modules/Currencies/app/Providers/Providers/RouteServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace modules\Sales\app\Providers\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Route::prefix('currencies/api')
+            ->middleware('api')
+            ->group(__DIR__ . '/../../routes/api.php');
+    }
+}
---
+++ modules/Suppliers/app/Providers/Providers/AppServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace modules\Sales\app\Providers\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->register(\Modules\Authentication\app\Providers\RouteServiceProvider::class);
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+    }
+}
